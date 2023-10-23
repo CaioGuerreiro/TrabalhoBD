7bdb1a8c8811b5d5f7737d527990180eea8b814b
Suyane <suyanecarvalhof@gmail.com>
2023-10-23 19:36:12 -0300

    adicionado tela ClienteDetalhadoActivity e onclicklistenner

--- app/src/main/java/com/example/trabalhobd/controller/ClienteController.java
+++ app/src/main/java/com/example/trabalhobd/controller/ClienteController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class ClienteController extends AppDataBase implements ICrud<Cliente> {
 
-
+//coomentqario inutil
     ContentValues dadoDoObjeto;
     public ClienteController(Context context) {
         super(context);
